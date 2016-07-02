d8c34736e39d623affa31cd5a8a701bc5023f054
Philip <philip.pregler@gmx.de>
2016-07-02 02:03:19 +0200

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('raidplanerCtrl', function($scope) {
+
+})
+   
+.controller('rosterCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+ 
